b3dbc035b0ba2c0329ffa02f1848c411d9adbce3
bb912 <brettbissey@gmail.com>
2020-05-21 00:31:37 -0400

    continued working on routing and API functions

--- LAMP/LAMPAPI/app.py
+++ LAMP/LAMPAPI/app.py
@@ -15,44 +15,68 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
+# HOME
 @app.route('/', methods=['GET', 'POST'])
 def home():
     """ Session control"""
     if not session.get('logged_in'):
         return render_template('index.html')
     else:
-        if request.method == 'POST':
-            username = getname(request.form['username'])
-            return render_template('index.html', data=getfollowedby(username))
+        # what to do here?
+
         return render_template('index.html')
 
-
+# LOGIN PAGE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """Login Form"""
     if request.method == 'GET':
+        # NEED THIS HTML
         return render_template('login.html')
-    else:
-        name = request.form['username']
+
+    else if request.method == 'POST':
+        name = request.form['login']
         passw = request.form['password']
         try:
-            data = session.query(User).filter_by(username=name, password=passw).first()
-            if data is not None:
+            user_search = session.query(User).filter_by(login=name, password=passw).first()
+            if user_search is not None:
                 session['logged_in'] = True
                 return redirect(url_for('home'))
             else:
                 return "Incorrect Username or Password"
         except:
-            return "Dont Login"
+            return "Incorrect Username or Password"
 
 
+# REGISTER PAGE
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register Form"""
+    if request.method == 'GET':
+        # NEED THIS HTML
+        return render_template('register.html')
 
+    else if request.method == 'POST':
+        name = request.form['username']
+        passw = request.form['password']
+        first = request.form['firstName']
+        last = request.form['lastName']
 
-
-
-
-
+        try:
+            user_search = session.query(User).filter_by(username=name).first()
+            if user_search is not None:
+                return "username is already in use, please enter another"
+            else:
+                new_user = User(first_name=first,
+                                  last_name=last,
+                                  login=name,
+                                  password=passw
+                                  )
+                session.add(new_user)
+                session.commit()
+                return redirect(url_for('login'))
+        except:
+            return "Incorrect Username or Password"
 
 
 @app.errorhandler(404)
@@ -72,27 +96,35 @@ def not_found(error):
 @app.route('/user/<int:user_id>/', )
 def showContactsForUser(user_id):
     contacts = session.query(Contact).filter_by(user_id=user_id)
+
+    # send the contacts with the html
+    # so the index.html template can display the contacts
     return render_template('index.html', contacts=contacts)
 
-
-# This will let us Create a new book and save it in our database
-@app.route('/books/new/', methods=['GET', 'POST'])
-def newBook():
+# should we pass in a user_id here?
+# This will let us Create a new contact and save it in our contacts database
+@app.route('/user/<int:user_id>/add_contact/', methods=['GET', 'POST'])
+def newContact(user_id):
     if request.method == 'POST':
-        newBook = Book(title=request.form['name'],
-                       author=request.form['author'],
-                       genre=request.form['genre'])
-        session.add(newBook)
+        new_contact = Contact(first_name=request.form['FirstName'],
+                          last_name=request.form['lastName'],
+                          phone=request.form['phone'],
+                          email=request.form['email'],
+                          user_id = user_id)
+
+        session.add(new_contact)
         session.commit()
-        return redirect(url_for('showBooks'))
+        # do we need to pass our user_id here. showing contacts after we add one
+        return redirect(url_for('showContactsForUser'))
     else:
-        return render_template('newBook.html')
+        # if GET go to the add_contact html, if template, send info here
+        return render_template('add_contact.html')
 
 
-# This will let us Update our books and save it in our database
-@app.route("/books/<int:book_id>/edit/", methods=['GET', 'POST'])
-def editBook(book_id):
-    editedBook = session.query(Book).filter_by(id=book_id).one()
+# Do we even need this routing or do we just need the API below?
+@app.route("/user/<int:user_id>/<int:contact_id>", methods=['GET', 'POST'])
+def editBook(user_id, contact_id):
+    edited_contact = session.query(Book).filter_by(id=contact_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedBook.title = request.form['name']
@@ -101,6 +133,9 @@ def editBook(book_id):
         return render_template('editBook.html', book=editedBook)
 
 
+
+
+# do we need something like this for contacts or is the delete in API sufficient?
 # This will let us Delete our book
 @app.route('/books/<int:book_id>/delete/', methods=['GET', 'POST'])
 def deleteBook(book_id):
@@ -142,6 +177,8 @@ def delete_contact(id):
     session.delete(contact_to_delete)
     session.commit()
     return "Removed contact with id %s" % contact_id
+
+
 
 # update an existing contact
 def update_contact(contact_id, first_name, last_name, phone, email):
